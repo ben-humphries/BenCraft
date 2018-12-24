2a2aaf37a0b05bc3e260d7b948bee4609788c621
Ben Humphries <benfhumphries@gmail.com>
2018-12-24 13:52:12 -0500

    Fixed glm

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -6,6 +6,10 @@
 #include <string>
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Shader.h"
 
 #define WINDOW_WIDTH 800
